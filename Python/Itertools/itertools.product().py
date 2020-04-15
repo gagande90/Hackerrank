de03312bce1398232ba4111bed751699771dbfb2
Gagan <49888313+gagande90@users.noreply.github.com>
2020-04-15 12:48:13 +1000

    Update itertools.product().py
    Updated the code to the latest supported version of python

--- Python/Itertools/itertools.product().py
+++ Python/Itertools/itertools.product().py
@@ -31,10 +31,10 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 from itertools import product
 
-A = raw_input().split()
+A = input().split()
 A = list(map(int, A))
-B = raw_input().split()
+B = input().split()
 B = list(map(int, B))
 
 for i in product(A, B):
-    print i,
+    print(i)
